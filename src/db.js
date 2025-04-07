cb3bfa5c866cd440f44e961c0b1a43662a0f3ed6
CrissP29 <pincaycriss29@gmail.com>
2025-04-07 16:56:19 -0500

    cambio bd

--- src/db.js
+++ src/db.js
@@ -1,9 +1,17 @@
-const {Pool} = require('pg')
-const pool =new  Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'Damm123',
-    database: 'moduloss',
-    port: '5432'
-})
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: "root",
+  host: "dpg-cvkrgi3e5dus73bt6ji0-a.oregon-postgres.render.com",
+  database: "syllabus_41et",
+  password: "LIfjDoBnekl6DK1m2I6s2jJbeRs9LTnk",
+  port: 5432,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+  keepAlive: true,
+  connectionTimeoutMillis: 120000,
+});
+
 module.exports = pool;
