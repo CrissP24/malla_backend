814e0b94355b99f8d83be0ebb1434119e9ea2fea
CRISTHOPER DAMIAN PINCAY ALAY <128093343+CrissP24@users.noreply.github.com>
2025-07-16 20:17:42 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,17 +1,9 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: "root",
-  host: "dpg-cvkrgi3e5dus73bt6ji0-a.oregon-postgres.render.com",
-  database: "syllabus_41et",
-  password: "LIfjDoBnekl6DK1m2I6s2jJbeRs9LTnk",
-  port: 5432,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-  keepAlive: true,
-  connectionTimeoutMillis: 120000,
-});
-
+const {Pool} = require('pg')
+const pool =new  Pool({
+    user: 'syllabus2025_owner',
+    host: 'ep-royal-sound-acuhwauy-pooler.sa-east-1.aws.neon.tech',
+    password: 'npg_9dZ3ouCacVhN',
+    database: 'msyllabus2025',
+    port: '5432'
+})
 module.exports = pool;
